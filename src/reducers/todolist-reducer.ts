07400cbcfefd91068c66c6b774816a8ff734cec0
slice of code <slice_of_code@mail.ru>
2023-08-10 21:01:42 +0400

    четвёртое занятие вторника с Игорем

---
+++ src/reducers/todolist-reducer.ts
@@ -0,0 +1,9 @@
+export const TodolistReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case "XXX": {
+            return state
+        }
+        default: return state
+    }
+}
+
